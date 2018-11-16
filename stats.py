7f5330e68b778673c8634f40928fef39d90c5bdd
joelpires <joelrogao@hotmail.com>
2018-11-16 16:13:43 +0000

    remote postgres databse connection

---
+++ stats.py
@@ -0,0 +1,72 @@
+"""
+Exploratory Analysis Tools for CDR Dataset
+November 2018
+Joel Pires
+"""
+from ConfigParser import ConfigParser
+
+__author__ = 'Joel Pires'
+__date__ = 'November 2018'
+
+import psycopg2
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+""" function that will parser the database.ini """
+def config(filename='database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
+""" Connect to the PostgreSQL database server """
+def connect():
+
+    conn = None
+    try:
+        # read connection parameters
+        params = config()
+
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**params)
+
+        # create a cursor
+        cur = conn.cursor()
+
+        # execute a statement
+        print('PostgreSQL database version:')
+        cur.execute('SELECT version()')
+
+        # display the PostgreSQL database server version
+        db_version = cur.fetchone()
+        print(db_version)
+
+        # close the communication with the PostgreSQL
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+            print('Database connection closed.')
+
+def main():
+    connect()
+
+
+if __name__ == '__main__':
+    main()
